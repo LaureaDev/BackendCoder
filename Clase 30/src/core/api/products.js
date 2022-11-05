d18b45d4e2a0d79d7f03e1b6e0084c0a421e9922
LaureanoNatalutti <79203128+Laureano2279@users.noreply.github.com>
2022-11-04 22:19:54 -0300

    Desafio completo

---
+++ Clase 30/src/core/api/products.js
@@ -0,0 +1,8 @@
+
+const ContenedorMongoDB = require('../contenedores/ContenedorMongoDB.js');
+const config = require('../../config/config.js');
+const { productschema } = require('../schemas/schemas.js');
+
+const productosApi = new ContenedorMongoDB('products', productschema, config);
+
+module.exports = productosApi;
